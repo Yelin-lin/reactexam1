b3ecaa52f43dc137a21d32feb83eb2b508f24de9
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-06-29 09:57:08 +0900

    Create: 시험내용으로 코드 모두 바꿈
    시험 내용으로 코드 다 다시 짬

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,58 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
-import ToDoItem from './components/ToDoItem';
 
 function App() {
 
   //기본 데이터
   const [toDo, setToDo] = useState([
-    {id:1, title:"리액트", content:"공부하자", isDone: false},
-    {id:2, title:"vercel", content:"해보자", isDone: false},
+    {id: 1, title: "리액트 공부"},
+    {id: 2, title: "배포 공부"},
   ])
 
-  //title과 content를 입력받는 걸 render해주기 위함
+  //title 입력을 알아차리고 저장하기 위함
   const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
 
-  //title입력받기 위함
   const titleChangeHandler = (event) => {
     setTitle(event.target.value);
   }
 
-  //content입력받기 위함
-  const contentChangeHandler = (event) => {
-    setContent(event.target.value);
-  }
-
-  //추가하기 버튼 handler
-  const clickAddButtonHandler = () => {
+  //추가하기 버튼 눌렀을 때 handler
+  const clickAddButtonHandler = () =>{
     const newToDo = {
       id: Date.now(),
       title,
-      content,
-      isDone: false,
     }
 
     setToDo([...toDo, newToDo])
 
-    setTitle('');
-    setContent('');
-  }
-
-  //삭제버튼 handler
-  const clickRemoveButtonHandler = (id) => {
-    const newToDo = toDo.filter((item)=>item.id!==id);
-    setToDo(newToDo)
-  }
-
-  //완료 버튼 눌렀을 때 
-  //완료 <-> 취소
-  const clickDoneButtonHandler = (id, isDone) =>{
-    const upDateToDo = toDo.map((item)=>{
-
-      if(item.id === id){
-        return {...item, isDone: !isDone}
-      }
-      return item;
-    })
-
-    setToDo(upDateToDo)
+    setTitle('')
   }
 
   return (
-    <div>
-      <div className='container'>
-        <div>My Todo List</div>
-        <div>React</div>
-      </div>
-      
-      <div className='inputTodoBox'>
-        <div className='inputTitleBox'>
-          제목 &nbsp;
-          <input
-            value={title}
-            onChange={titleChangeHandler}
-            />
-        </div>
-
-        <div className='inputContentBox'>
-          내용 &nbsp;
-          <input
-            value={content}
-            onChange={contentChangeHandler}
-            />
-        </div>
-
+    <div className='container'>
+      {/* 입력div 부분*/}
+      <div>
+        <input 
+        ype="text"
+        value={title}
+        onChange={titleChangeHandler}/>
         <button onClick={clickAddButtonHandler}>추가하기</button>
-
-      </div>
-      <div className='toDoListBox'>
-        <div className='toDoBox'>
-          <div>Working.. 🔥</div>
-          <div className='toDoItemList'>
-            {toDo
-            .filter((item)=>item.isDone===false)
-            .map((item)=>{
-              return <ToDoItem key={item.id} item={item} clickRemoveButtonHandler={clickRemoveButtonHandler} clickDoneButtonHandler={clickDoneButtonHandler}>완료</ToDoItem>
-            })}
-          </div>
-        </div>
-
-        <div className='toDoBox'>
-          <div>Done..! 🎉</div>
-          <div className='toDoItemList'>
-            {toDo
-            .filter((item)=>item.isDone===true)
-            .map((item)=>{
-              return <ToDoItem key={item.id} item={item} clickRemoveButtonHandler={clickRemoveButtonHandler} clickDoneButtonHandler={clickDoneButtonHandler}>취소</ToDoItem>
-            })}
-          </div>
-        </div>
       </div>
 
+      {/* Todo List div 부분 */}
+      <div className='toDoListText'>ToDo List</div>
+      <div className='toDoBoxs'>
+        {toDo
+        .map((item)=>{
+          return (
+            <div className='toDoBox' key={item.id}>
+              {item.title}
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
